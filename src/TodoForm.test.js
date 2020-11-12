da4cde4c378093ca2eed84be0fa0902d96c65219
hanjiyeon <jiyeon53885@gmail.com>
2020-11-12 14:36:09 +0900

    6. 리팩토링

--- src/TodoForm.test.js
+++ src/TodoForm.test.js
@@ -3,15 +3,25 @@ import { render, fireEvent } from "@testing-library/react";
 import TodoForm from "./TodoForm";
 
 describe("<TodoFrom />", () => {
+    const setup = (props = {}) => {
+        const utils = render(<TodoForm {...props} />);
+        const { getByText, getByPlaceholderText } = utils;
+        const input = getByPlaceholderText("할 일을 입력하세요");
+        const button = getByText("등록");
+        return {
+            ...utils,
+            input,
+            button,
+        };
+    };
     it("has input and a button", () => {
-        const { getByText, getByPlaceholderText } = render(<TodoForm />);
-        getByPlaceholderText("할 일을 입력하세요"); //input이 있는지 확인
-        getByText("등록"); //button이 있는지 확인
+        const { input, button } = setup();
+        expect(input).toBeTruthy(); //해당 값이 truthy
+        expect(button).toBeTruthy();
     });
 
     it("changes input", () => {
-        const { getByPlaceholderText } = render(<TodoForm />);
-        const input = getByPlaceholderText("할 일을 입력하세요");
+        const { input } = setup();
         fireEvent.change(input, {
             target: {
                 value: "TDD 배우기",
@@ -24,9 +34,7 @@ describe("<TodoFrom />", () => {
         /*여기서 사용한 jest.fn() 은 jest 에서 제공하는 mock 함수입니다. 
         이 함수를 사용하면 이 함수가 호출 된 다음 toBeCalled 또는 toBeCalledWith 같은 matcher 를 사용해서 함수가 호출됐는지, 
         호출 됐다면 어떤 파라미터로 호출 됐는지 이런 것들을 쉽게 확인 할 수 있습니다. */
-        const { getByText, getByPlaceholderText } = render(<TodoForm onInsert={onInsert} />);
-        const input = getByPlaceholderText("할 일을 입력하세요");
-        const button = getByText("등록");
+        const { input, button } = setup({ onInsert }); //props가 필요할 땐 이렇게 직접 파라미터로 전달
         //수정하고
         fireEvent.change(input, {
             target: {
